f7e5c14a38df53908745eff4ffcb59a0dc79a268
memmem0 <memmem0@hanmail.net>
2016-12-07 10:53:42 +0900

    no message

--- main.rb
+++ main.rb
@@ -1,2 +1,18 @@
-  int a;
-  return 0;
+# 메뉴의 인덱스
+$menu_index = 0
+
+# 버튼의 갯수
+$menu_size = 4
+
+# 메뉴 인덱스 값을 증가시킨다
+def index_up
+  $menu_index = ($menu_index + 1) % $menu_size
+end
+
+# 메뉴 인덱스 값을 감소시킨다
+def index_down
+  $menu_index = ($menu_index - 1) % $menu_size
+end
+
+# 메뉴 인덱스 값을 8번 반복적으로 증가시킨다
+8.times {|i| puts index_up}
